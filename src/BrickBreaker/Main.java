2b599f4847a85c239e1feccc540490eadb223199
Asif Anik <45178199+asifahmed90@users.noreply.github.com>
2019-06-22 01:08:49 -0500

    Add files via upload

---
+++ src/BrickBreaker/Main.java
@@ -0,0 +1,22 @@
+package BrickBreaker;
+
+import java.awt.Color;
+
+import javax.swing.JFrame;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame obj=new JFrame();
+        GamePlay gamePlay = new GamePlay();
+
+        obj.setBounds(10, 10, 700, 600);
+        obj.setTitle("Breakout Ball");
+        obj.setResizable(false);
+        obj.setVisible(true);
+        obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        obj.add(gamePlay);
+        obj.setVisible(true);
+
+    }
+
+}
